c6b94a74def523b330eb8705495dc88839c3cb18
Lizbeth Villalvazo <lizbeth.villalvazo@grupoabsa.com>
2019-01-07 18:04:18 +0000

    [FIX] sintaxys errors from models, partner and manifest

--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -2,11 +2,12 @@
 
 from odoo import models, fields, api
 
-class Partner(models.Model):
-   # _name = 'openacademy.course'
-   _inherit = 'res.partner'
 
-   instructor = fields.Boolean(defailt=False)
-   session_ids = fields.Many2many ('openacademy.session', string="Attended Sessions", readonly=True)
-   other_field = fields.Boolean(default=True)
-   other_field2 = fields.Boolean(default=True)
+class Partner(models.Model):
+    # _name = 'openacademy.course'
+    _inherit = 'res.partner'
+
+    instructor = fields.Boolean(defailt=False)
+    session_ids = fields.Many2many ('openacademy.session', string="Attended Sessions", readonly=True)
+    other_field = fields.Boolean(default=True)
+    other_field2 = fields.Boolean(default=True)
