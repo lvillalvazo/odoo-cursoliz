51e81d856c7a00e19edb3e33dc50d92ad0a240d5
Lizbeth Villalvazo <lizbeth.villalvazo@grupoabsa.com>
2019-01-07 21:14:13 +0000

    [REF] correction in __manifest.py

--- openacademy/controllers/controllers.py
+++ openacademy/controllers/controllers.py
@@ -10,12 +10,10 @@ from odoo import http
 #     def list(self, **kw):
 #         return http.request.render('openacademy.listing', {
 #             'root': '/openacademy/openacademy',
-#             'objects': http.request.env['openacademy.openacademy']
-# .search([]),
+#             'objects': http.request.env['openacademy.openacademy'].search([]),
 #         })
 
-#     @http.route('/openacademy/openacademy/objects/<model("openacademy.openacademy"):obj>/',
-#  auth='public')
+#     @http.route('/openacademy/openacademy/objects/<model("openacademy.openacademy"):obj>/',auth='public')
 #     def object(self, obj, **kw):
 #         return http.request.render('openacademy.object', {
 #             'object': obj
