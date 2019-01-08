501f60065d66177b6c033287e64034afdc8793c2
Lizbeth Villalvazo <lizbeth.villalvazo@grupoabsa.com>
2019-01-08 19:01:54 +0000

    Fixing Flake8 errors: partner and models 2

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -85,7 +85,7 @@ class Session(models.Model):
         for record in self:
             record.attendees_count = len(record.attendee_ids)
     attendees_count = fields.Integer(
-            compute='_get_attendees_count', store=True)
+                    compute='_get_attendees_count', store=True)
 
 #    @api.depends('attendee_ids')
 #    def _get_attendees_count(self):
@@ -113,17 +113,17 @@ class Session(models.Model):
     def _verify_valid_seats(self):
         if self.filtered(lambda r: r.seats < 0):
             self.active = False
-                return {
-                'warning': {'title': _("Incorrect 'seats' value"),
-                'message': (
-                "The number of available seats may not be negative"), }
+        return {
+            'warning': {'title': _("Incorrect 'seats' value"),
+            'message': (
+            "The number of available seats may not be negative"), }
             }
         if self.seats < len(self.attendee_ids):
             self.active = False
-                return {
-                'warning': {'title': ("Too many attendees"),
-                'message': _("Increase seats or remove excess attendees"), }
-            }
+        return {
+           'warning': {'title': ("Too many attendees"),
+            'message': _("Increase seats or remove excess attendees"), }
+        }
         self.active = True
 
     @api.constrains('instructor_id', 'attendee_ids')
